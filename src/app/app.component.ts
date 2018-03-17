0c25deb5bab6d48548cc57b11d8cf5522dd97b3f
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2018-03-17 13:51:38 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { CartPage } from '../pages/cart/cart';
+import { CloudPage } from '../pages/cloud/cloud';
+
+
+import { HomePage } from '../pages/home/home';
+
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) navCtrl: Nav;
+    rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+  goToHome(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(HomePage);
+  }goToCart(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(CartPage);
+  }goToCloud(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(CloudPage);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { CartPage } from '../pages/cart/cart';
+import { CloudPage } from '../pages/cloud/cloud';
+import {  } from '../pages//';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    CartPage,
+    CloudPage,
+    
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    CartPage,
+    CloudPage,
+    
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
